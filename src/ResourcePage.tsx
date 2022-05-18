1a09b16e3d5813ea0a7d95d39caf66e22a7366f1
Joep Meindertsma <joep@ontola.io>
2022-05-18 14:54:12 +0200

    description

--- src/ResourcePage.tsx
+++ src/ResourcePage.tsx
@@ -1,4 +1,4 @@
-import { useResource, useTitle } from "@tomic/react";
+import { properties, useResource, useString, useTitle } from "@tomic/react";
 
 interface ResourcePageProps{
   url: string,
@@ -9,8 +9,10 @@ export function ResourcePage ({
 }: ResourcePageProps) {
   const resource = useResource(url)
   const title = useTitle(resource)
+  const [description] = useString(resource, properties.description)
 
   return <div>Resource: {url}
   <h1>{title}</h1>
+  <p>{description}</p>
   </div>
 }
